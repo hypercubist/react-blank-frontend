a109f84ba214347aca783ae947c065f89d3bb89c
hypercubist <qpinguz@gmail.com>
2022-06-20 23:21:18 +0900

    cors problem solved

--- src/apis/loginApi.ts
+++ src/apis/loginApi.ts
@@ -1,7 +1,9 @@
 const BLANK_SERVER_URL = "http://localhost:8900/api/v1";
 
 export async function fetchLoginUser() {
-  const response = await fetch(`${BLANK_SERVER_URL}/user`);
+  const response = await fetch(`${BLANK_SERVER_URL}/user`, {
+    credentials: "include",
+  });
   console.log(response);
   if (response.status === 200) {
     return await response.json();
